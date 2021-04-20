a5f56b553959a362f3f286913c2bb4d653046e96
Michael Schubert <schu@schu.io>
2021-04-20 17:35:28 +0200

    Add trix editor assets
    https://github.com/basecamp/trix

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,7 @@ require('laravel-mix-svelte');
 // editorconfig-checker-enable
 
 mix.js('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', 'public/js')
+    .js('node_modules/trix/dist/trix.js', 'public/js')
     .js('resources/js/app.js', 'public/js')
     .js('resources/js/DeckForm.js', 'public/js')
     .js('resources/js/SessionView.js', 'public/js')
