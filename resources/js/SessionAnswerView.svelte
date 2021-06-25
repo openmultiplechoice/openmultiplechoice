a6a4a8492c3cca525748bd368cdd24d718167492
Michael Schubert <schu@schu.io>
2021-06-25 18:17:23 +0200

    SessionAnswerView: give excluded answer options red background
    Users can mark an answer option as "probably wrong" by clicking a
    "close" button on an answer. Currently, this does strike through the
    text of the answer, which is not a very obvious marker. Instead, give
    the answer a red background and keep the text readable at the same time.

--- resources/js/SessionAnswerView.svelte
+++ resources/js/SessionAnswerView.svelte
@@ -8,6 +8,7 @@
     export let isCorrectAnswer;
     export let submitAnswer;
 
+    var answerCancelledIndicator;
     var answerStatusIndicator;
     var cancelled;
 
@@ -23,6 +24,12 @@
             }
         }
     };
+
+    $: if (cancelled) {
+        answerCancelledIndicator = 'bg-cancelled-answer';
+    } else {
+        answerCancelledIndicator = '';
+    }
 </script>
 
 <style>
@@ -35,9 +42,13 @@
 .bg-light-hover:hover {
     background: #f8f9fa;
 }
+.bg-cancelled-answer {
+    color: #495057 !important;
+    background: #f8d7da !important;
+}
 </style>
 
-<div id="answer{answer.id}" class="row border-start border-3 {answerStatusIndicator} m-1 pt-2 { hasAnswer ? '' : 'bg-light-hover'}">
+<div id="answer{answer.id}" class="row border-start border-3 {answerStatusIndicator} {answerCancelledIndicator} m-1 pt-2 { hasAnswer ? '' : 'bg-light-hover'}">
     {#if !hasAnswer}
         <div on:click={() => submitAnswer(answer.id)} class="col-1 border-start-3 cursor-pointer">
             <p class="badge text-dark">{badgeText}</p>
@@ -50,11 +61,11 @@
 
     {#if !hasAnswer}
         <div on:click={() => submitAnswer(answer.id)} class="col-10 cursor-pointer">
-            <p class="p-1" class:text-decoration-line-through={cancelled} class:text-muted={cancelled}>{@html DOMPurify.sanitize(answer.text)}</p>
+            <p class="p-1">{@html DOMPurify.sanitize(answer.text)}</p>
         </div>
     {:else}
         <div class="col-10">
-            <p class="p-1" class:text-decoration-line-through={cancelled} class:text-muted={cancelled}>{@html DOMPurify.sanitize(answer.text)}</p>
+            <p class="p-1">{@html DOMPurify.sanitize(answer.text)}</p>
         </div>
     {/if}
 
