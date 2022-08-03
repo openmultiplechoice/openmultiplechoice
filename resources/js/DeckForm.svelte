81cb996fde6eee7b0dccfba8f5348350d0f42993
Michael Schubert <schu@schu.io>
2022-08-03 17:04:56 +0200

    DeckForm: put question index into separate component

--- resources/js/DeckForm.svelte
+++ resources/js/DeckForm.svelte
@@ -1,21 +1,25 @@
 <script>
-    import DOMPurify from "dompurify";
     import { onMount } from "svelte";
     import QuestionForm from "./QuestionForm.svelte";
+    import DeckFormQuestionIndex from "./DeckFormQuestionIndex.svelte";
 
     export let id;
     export let name;
 
-    let questions = [];
-    let currentQuestion;
+    let data = { questions: [] };
+
+    $: currentQuestion = data
+        ? data.questions.find((q) => q.id === data.current_question_id)
+        : null;
 
     onMount(() => {
         axios
             .get("/api/decks/" + id + "/questions")
             .then(function (response) {
-                questions = response.data;
-                if (questions.length > 0) {
-                    currentQuestion = questions[questions.length - 1];
+                data.questions = response.data;
+                if (data.questions.length > 0) {
+                    data.current_question_id =
+                        data.questions[data.questions.length - 1].id;
                 }
             })
             .catch(function (error) {
@@ -37,8 +41,8 @@
             .post("/api/decks/" + id + "/questions", newQuestion)
             .then(function (response) {
                 newQuestion.id = response.data.id;
-                questions = [...questions, newQuestion];
-                currentQuestion = newQuestion;
+                data.questions = [...data.questions, newQuestion];
+                data.current_question_id = newQuestion.id;
             })
             .catch(function (error) {
                 alert(error);
@@ -49,7 +53,13 @@
         axios
             .delete("/api/decks/" + id + "/questions/" + questionId)
             .then(function (response) {
-                questions = questions.filter((q) => q.id !== questionId);
+                data.questions = data.questions.filter(
+                    (q) => q.id !== questionId
+                );
+                if (data.questions.length > 0) {
+                    data.current_question_id =
+                        data.questions[data.questions.length - 1].id;
+                }
             })
             .catch(function (error) {
                 alert(error);
@@ -70,46 +80,12 @@
 
 <div class="row">
     <div class="col-md-4">
+        <DeckFormQuestionIndex bind:data {handleQuestionRemove} />
         <div
             class="btn-group-vertical btn-group-sm"
             style="width: 100%;"
             role="group"
-            aria-label="Vertical button group">
-            {#each [...questions].reverse() as question}
-                <div class="btn-group" role="group">
-                    <span class="btn-index">
-                        <button
-                            on:click|preventDefault={() => {
-                                currentQuestion = question;
-                            }}
-                            type="button"
-                            class="btn btn-light btn-index text-start"
-                            ><span class="badge bg-secondary me-1"
-                                >ID: {question.id}</span>
-                            {DOMPurify.sanitize(question.text).replace(
-                                /<\/?[^>]+(>|$)/g,
-                                ""
-                            )}</button>
-                    </span>
-                    <button
-                        type="button"
-                        class="btn btn-outline-dark dropdown-toggle dropdown-toggle-split"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        <span class="visually-hidden">Toggle Dropdown</span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li
-                            class="dropdown-item"
-                            on:click|preventDefault={() => {
-                                handleQuestionRemove(question.id);
-                            }}>
-                            Remove question
-                        </li>
-                    </ul>
-                </div>
-            {/each}
-        </div>
+            aria-label="Vertical button group" />
     </div>
     <div class="col-md-8">
         {#if currentQuestion}
@@ -119,13 +95,3 @@
         {/if}
     </div>
 </div>
-
-<style>
-    .btn-index {
-        width: 100%;
-        display: block;
-        overflow: hidden !important;
-        white-space: nowrap;
-        text-overflow: ellipsis !important;
-    }
-</style>
