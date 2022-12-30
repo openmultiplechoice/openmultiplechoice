0a3fa0b50a03dc8695d142b3ff09596d20bcc1d4
Michael Schubert <schu@schu.io>
2022-12-30 10:11:28 +0100

    Add popper.min.js
    Not used yet.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
+    <script src="{{ asset('js/popper.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('js/trix.min.js') }}"></script>
     <script src="{{ asset('js/app.js') }}"></script>
