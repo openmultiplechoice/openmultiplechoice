dfe75dca1dc4e2c019152cfd739b30ede0817c1b
Michael Schubert <schu@schu.io>
2023-01-28 19:18:59 +0100

    Remove redundant `sessionProgressPercentage` code
    Use a single implementation of `sessionProgressPercentage` from
    `StatsHelper.js`.

--- resources/js/NewSessionView.svelte
+++ resources/js/NewSessionView.svelte
@@ -2,6 +2,8 @@
     import { onMount } from "svelte";
     import { format, parseISO } from "date-fns";
 
+    import { sessionProgressPercentage } from "./StatsHelper.js";
+
     let decks = [];
     let modules = [];
     let subjects = [];
@@ -128,55 +130,6 @@
             .catch(function (error) {
                 alert(error);
             });
-    }
-
-    function sessionProgressPercentage(numQuestions, answerChoices) {
-        const numCorrectAnswers = answerChoices.filter(
-            (c) => c.is_correct && !c.help_used
-        ).length;
-        const numCorrectAnswersWithHelp = answerChoices.filter(
-            (c) => c.is_correct && c.help_used
-        ).length;
-        const numIncorrectAnswers = answerChoices.filter(
-            (c) => !c.is_correct
-        ).length;
-
-        const percentage = {
-            correct: Math.round(100 * (numCorrectAnswers / numQuestions)),
-            correctWithHelp: Math.round(
-                100 * (numCorrectAnswersWithHelp / numQuestions)
-            ),
-            incorrect: Math.round(100 * (numIncorrectAnswers / numQuestions)),
-            unanswered: Math.round(
-                100 *
-                    ((numQuestions -
-                        numCorrectAnswers -
-                        numCorrectAnswersWithHelp -
-                        numIncorrectAnswers) /
-                        numQuestions)
-            ),
-        };
-
-        // Make sure to have 100% in total
-        if (
-            percentage.correct +
-                percentage.correctWithHelp +
-                percentage.incorrect +
-                percentage.unanswered !=
-            100
-        ) {
-            if (percentage.unanswered > 0) {
-                percentage.unanswered = percentage.unanswered + 1;
-            } else if (percentage.incorrect > 0) {
-                percentage.incorrect = percentage.incorrect + 1;
-            } else if (percentage.correctWithHelp > 0) {
-                percentage.incorrectWithHelp = percentage.correctWithHelp + 1;
-            } else if (percentage.correct > 0) {
-                percentage.correct = percentage.correct + 1;
-            }
-        }
-
-        return percentage;
     }
 </script>
 
