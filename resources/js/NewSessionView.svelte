f12c89b001ecdc211998aeabb4f0c8f860b6da52
Michael Schubert <schu@schu.io>
2023-01-31 19:17:10 +0100

    NewSessionView: add missing margin to subject/module list

--- resources/js/NewSessionView.svelte
+++ resources/js/NewSessionView.svelte
@@ -138,7 +138,7 @@
 
 {#if decks}
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-4 mb-2">
             <ul class="list-group">
                 {#each subjects as subject}
                     <button
