1b08141018456783d91f417de3e1278d1c2b395e
Michael Schubert <schu@schu.io>
2023-02-07 18:44:53 +0100

    SessionQuestionIndexView: use `scrollIntoView` to show current question in view

--- resources/js/SessionQuestionIndexView.svelte
+++ resources/js/SessionQuestionIndexView.svelte
@@ -9,6 +9,20 @@
             (q) => q.id == data.session.current_question_id
         ) + 1;
 
+    $: data.session.current_question_id,
+        (() => {
+            var e = document.getElementById(
+                "question" + data.session.current_question_id
+            );
+            if (e) {
+                e.scrollIntoView({
+                    behavior: "smooth",
+                    block: "nearest",
+                    inline: "center",
+                });
+            }
+        })();
+
     $: answerChoiceIndicator = function (question) {
         var answerChoice = data.session.answer_choices.find(
             (e) => e.question_id === question.id
@@ -33,31 +47,35 @@
     {indexCurrentQuestion}/{numberQuestions}
 </p>
 
-<ul class="list-group ">
-    {#each data.deck.questions as question, index}
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <li
-            on:click={() => (data.session.current_question_id = question.id)}
-            class="list-group-item list-group-item-action {question.id ===
-            data.session.current_question_id
-                ? 'list-group-item-dark'
-                : 'list-group-item-light'}">
-            <small>
-                {@html answerChoiceIndicator(question)}
-                {#if question.text}
-                    {DOMPurify.sanitize(question.text).replace(
-                        /<\/?[^>]+(>|$)/g,
-                        ""
-                    )}
-                {:else}
-                    Question Nr. {index + 1}
-                {/if}
-            </small>
-        </li>
-    {:else}
-        <li>Loading ...</li>
-    {/each}
-</ul>
+<div class="overflow-scroll" style="max-height: 85vh;">
+    <ul class="list-group">
+        {#each data.deck.questions as question, index}
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <li
+                on:click={() =>
+                    (data.session.current_question_id = question.id)}
+                class="list-group-item list-group-item-action {question.id ===
+                data.session.current_question_id
+                    ? 'list-group-item-dark'
+                    : 'list-group-item-light'}"
+                id="question{question.id}">
+                <small>
+                    {@html answerChoiceIndicator(question)}
+                    {#if question.text}
+                        {DOMPurify.sanitize(question.text).replace(
+                            /<\/?[^>]+(>|$)/g,
+                            ""
+                        )}
+                    {:else}
+                        Question Nr. {index + 1}
+                    {/if}
+                </small>
+            </li>
+        {:else}
+            <li>Loading ...</li>
+        {/each}
+    </ul>
+</div>
 
 <style>
     li {
