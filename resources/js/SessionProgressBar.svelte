e3e1410a1354f9d8eb58606020e98ab626b8ade8
Michael Schubert <schu@schu.io>
2023-02-27 14:33:30 +0100

    SessionProgressBar: hide labels <= 4%
    On smaller screens a low percentage value takes only a small part of the
    progress bar, too small to hold two characters without overflowing. Hide
    small values in order to avoid "UI glitches".

--- resources/js/SessionProgressBar.svelte
+++ resources/js/SessionProgressBar.svelte
@@ -19,24 +19,24 @@
         class="progress-bar"
         role="progressbar"
         style="color: #155724; background-color: #d4edda; width: {percentage.correct}%">
-        {percentage.correct}%
+        {percentage.correct > 4 ? percentage.correct + "%" : ""}
     </div>
     <div
         class="progress-bar"
         role="progressbar"
         style="color: #664d03; background-color: #fff3cd; width: {percentage.correctWithHelp}%">
-        {percentage.correctWithHelp}%
+        {percentage.correctWithHelp > 4 ? percentage.correctWithHelp + "%" : ""}
     </div>
     <div
         class="progress-bar"
         role="progressbar"
         style="color: #721c24; background-color: #f8d7da; width: {percentage.incorrect}%">
-        {percentage.incorrect}%
+        {percentage.incorrect > 4 ? percentage.incorrect + "%" : ""}
     </div>
     <div
         class="progress-bar"
         role="progressbar"
         style="color: #1b1e21; background-color: #d6d8d9; width: {percentage.unanswered}%">
-        {percentage.unanswered}%
+        {percentage.unanswered > 4 ? percentage.unanswered + "%" : ""}
     </div>
 </div>
