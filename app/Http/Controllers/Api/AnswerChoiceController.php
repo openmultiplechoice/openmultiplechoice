9d2cd8ef7b59388de531a8ea20277343374eacfe
Michael Schubert <schu@schu.io>
2023-05-04 19:22:12 +0200

    Api/AnswerChoiceController: check for owner and duplicates
    Check whether the current user actually is the owner of the session.
    
    Also, make sure that there can be only one answer choice per question.
    
    Fixes #565

--- app/Http/Controllers/Api/AnswerChoiceController.php
+++ app/Http/Controllers/Api/AnswerChoiceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 use App\Models\AnswerChoice;
 use App\Models\Session;
@@ -11,10 +12,21 @@ class AnswerChoiceController extends Controller
 {
     public function store(Request $request, Session $session)
     {
-        $answerChoice = new AnswerChoice();
-        $answerChoice->fill($request->all());
-        $answerChoice->session_id = $session->id;
-        $answerChoice->save();
+        if (Auth::id() != $session->user_id) {
+            return response()->json(['error' => 'Forbidden'], 403);
+        }
+
+        $answerChoice = AnswerChoice::updateOrCreate(
+            [
+                'session_id' => $session->id,
+                'question_id' => $request->question_id,
+            ],
+            [
+                'answer_id' => $request->answer_id,
+                'help_used' => $request->help_used,
+                'is_correct' => $request->is_correct,
+            ]
+        );
 
         return response()->json($answerChoice);
     }
