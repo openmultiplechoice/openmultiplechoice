114f10ffaaf0911679025bc9378ee11e155a151c
Michael Schubert <schu@schu.io>
2023-05-27 09:07:49 +0200

    Api/AnswerChoiceController: attempt to avoid duplicate inserts
    Despite the change to `updateOrCreate` with
    9d2cd8ef7b59388de531a8ea20277343374eacfe a user had 3 answers for the
    same question in their table. While I couldn't reproduce the issue with
    a test script (see below), the redundant rows were inserted. Wrap the
    table update in a transaction.
    
    ```
    for i in 1 2 3 4 5; do
        curl -fsSL \
            -H 'Authorization: Bearer ...' \
            -H 'Content-Type: application/json' \
            --data '{"question_id": 3, "answer_id": 10, "is_correct": true}' \
            127.0.0.1:8000/api/sessions/8/answerchoices &
    done
    ```

--- app/Http/Controllers/Api/AnswerChoiceController.php
+++ app/Http/Controllers/Api/AnswerChoiceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 use App\Models\AnswerChoice;
 use App\Models\Session;
@@ -16,17 +17,23 @@ class AnswerChoiceController extends Controller
             return response()->json(['error' => 'Forbidden'], 403);
         }
 
-        $answerChoice = AnswerChoice::updateOrCreate(
-            [
-                'session_id' => $session->id,
-                'question_id' => $request->question_id,
-            ],
-            [
-                'answer_id' => $request->answer_id,
-                'help_used' => $request->help_used,
-                'is_correct' => $request->is_correct,
-            ]
-        );
+        try {
+            $answerChoice = DB::transaction(function () use ($request, $session) {
+                return AnswerChoice::updateOrCreate(
+                    [
+                        'session_id' => $session->id,
+                        'question_id' => $request->question_id,
+                    ],
+                    [
+                        'answer_id' => $request->answer_id,
+                        'help_used' => $request->help_used,
+                        'is_correct' => $request->is_correct,
+                    ]
+                );
+            }, 3);
+        } catch (Throwable $t) {
+            abort(500, 'Failed to save answer');
+        }
 
         return response()->json($answerChoice);
     }
