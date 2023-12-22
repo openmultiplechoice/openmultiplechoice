3081ff24455ac1e20b1ac2e4eff6b80c91a8cc43
Michael Schubert <schu@schu.io>
2023-12-22 18:25:56 +0100

    resources/views/info-single: add breadcrumb menu at top of page
    Also, use a bootstrap `display-6` class for the main header.

--- resources/views/info-single.blade.php
+++ resources/views/info-single.blade.php
@@ -6,7 +6,14 @@
 
 <div class="row">
     <div class="col-md">
-        <h1 class="h4">{{ $info->title }}</h1>
+        <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ url('info') }}">Info</a></li>
+            <li class="breadcrumb-item active" aria-current="page"></li>
+        </ol>
+        </nav>
+
+        <h1 class="display-6">{{ $info->title }}</h1>
         <p class="font-monospace"><small>{{ $info->created_at->format('d.m.Y') }}</small></p>
         <p>{!! $info->text !!}</p>
     </div>
