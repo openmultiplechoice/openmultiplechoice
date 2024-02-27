6d2ed1f412ee4430c276785383fc207802290ce7
Michael Schubert <schu@schu.io>
2024-02-27 01:05:04 +0100

    SessionProgressBar: make text bold
    Make the text bold for the values to be more readable. Also, only show
    the percent labels from >5% on (before >4%) to make the look less
    crammed on mobile devices with a low screen width.

--- resources/js/SessionProgressBar.svelte
+++ resources/js/SessionProgressBar.svelte
@@ -6,27 +6,27 @@
 
 <div class="progress" style="height: 20px;" title="{title}">
     <div
-        class="progress-bar bg-success text-dark"
+        class="progress-bar bg-success text-dark fw-bold"
         role="progressbar"
         style="width: {progressPercentage.correct}%">
-        {progressPercentage.correct > 4 ? progressPercentage.correct + "%" : ""}
+        {progressPercentage.correct > 5 ? progressPercentage.correct + "%" : ""}
     </div>
     <div
-        class="progress-bar bg-warning text-dark"
+        class="progress-bar bg-warning text-dark fw-bold"
         role="progressbar"
         style="width: {progressPercentage.correctWithHelp}%">
-        {progressPercentage.correctWithHelp > 4 ? progressPercentage.correctWithHelp + "%" : ""}
+        {progressPercentage.correctWithHelp > 5 ? progressPercentage.correctWithHelp + "%" : ""}
     </div>
     <div
-        class="progress-bar bg-danger text-dark"
+        class="progress-bar bg-danger text-dark fw-bold"
         role="progressbar"
         style="width: {progressPercentage.incorrect}%">
-        {progressPercentage.incorrect > 4 ? progressPercentage.incorrect + "%" : ""}
+        {progressPercentage.incorrect > 5 ? progressPercentage.incorrect + "%" : ""}
     </div>
     <div
-        class="progress-bar"
+        class="progress-bar fw-bold"
         role="progressbar"
         style="color: #1b1e21; background-color: #d6d8d9; width: {progressPercentage.unanswered}%">
-        {progressPercentage.unanswered > 4 ? progressPercentage.unanswered + "%" : ""}
+        {progressPercentage.unanswered > 5 ? progressPercentage.unanswered + "%" : ""}
     </div>
 </div>
