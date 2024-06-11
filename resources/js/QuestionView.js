5d6ad0f72c3f9a6e326ed2a42208715a9dfd434c
Michael Schubert <schu@schu.io>
2024-06-11 17:24:43 +0200

    QuestionView: fix broken page due to regression
    This is a fixup for 05d3cb41e37292308ec4edc5ba4c6d4fbdd5539c which did
    miss to update the question view to pass a `questionContext` for the
    `QuestionView` component.

--- resources/js/QuestionView.js
+++ resources/js/QuestionView.js
@@ -2,11 +2,21 @@ import SessionQuestionView from './SessionQuestionView.svelte';
 
 const questionViewEl = document.getElementById('QuestionView');
 
+const question = JSON.parse(questionViewEl.dataset.question);
+
 const f = new SessionQuestionView({
     target: questionViewEl,
     props: {
-        question: JSON.parse(questionViewEl.dataset.question),
-        questionAnswered: true,
+        question: question,
+        questionContext: {
+            isAnswered: true,
+            answerContext: question.answers.reduce((acc, obj) => {
+                acc[obj.id] = {
+                    isCorrectAnswer: obj.id === question.correct_answer_id,
+                };
+                return acc;
+            }, {}),
+        },
         // Set answerChoice to an "invalid" value. We don't
         // have an answer, here we merely want to show the
         // question
