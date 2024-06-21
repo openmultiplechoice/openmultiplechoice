f18b60c7b262d1b4d74f0cb944aa16ee5e0bd99d
Michael Schubert <schu@schu.io>
2024-06-21 16:09:20 +0200

    npm update

---
+++ public/build/assets/Messages-BXWJQHSA.js
@@ -0,0 +1,1 @@
+import{M as t}from"./Messages-CG_EV6Xq.js";import"./index-B80I9K_4.js";import"./each-DkBj6IvG.js";import"./purify.es-FL75tu_8.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./UserSettingsStore-BgSeteCb.js";import"./parseISO-Ds5nI7_t.js";const e=document.getElementById("MessagesView");new t({target:e,props:{questionId:e.dataset.questionId}});
