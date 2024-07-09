3c325b7ce99ac5f7c03f0255bb97d55f0eea25f2
levinuss <github@accounts.levinus.de>
2024-07-09 14:55:26 +0200

    Add view for questions that need review
    Adds a view that lists all questions with the `needs_review` or the
    `is_invalid` flag set and that are part of at least one public and
    writeable deck for users to discover and contribute improvements.
    
    Co-authored-by: Michael Schubert <schu@schu.io>

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -18,4 +18,24 @@ class QuestionController extends Controller
             'decks' => $decks,
         ]);
     }
+
+    public function indexReviewable()
+    {
+        // Select questions that need review and are in at least one public (writeable) deck.
+        $questions = Question::where(function ($q) {
+            $q->where('needs_review', true)
+                ->orWhere('is_invalid', true);
+        })
+            ->whereHas('decks', function ($q) {
+                $q->whereIn('access', ['public-rw-listed', 'public-rw']);
+            })
+            ->with(['decks' => function ($q) {
+                $q->whereIn('access', ['public-rw-listed', 'public-rw', 'public-ro'])
+                    ->with('module', 'module.subject');
+            }])
+            ->paginate(25);
+        return view('questions-reviewable', [
+            'questions' => $questions,
+        ]);
+    }
 }
