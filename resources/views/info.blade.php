a2b74f675230fd40abede52d20e480d1f0314459
Michael Schubert <schu@schu.io>
2024-07-19 23:42:14 +0200

    resources/views: replace `preg_split` with `Str::limit` for info alerts
    So far we have been using `preg_split` to limit the amount of shown text
    in info alert elements. Instead, use Laravel's `Str::limit` helper
    function to make it easier for users to recognize if text has been
    truncated (` (...)` appended).
    
    https://laravel.com/docs/11.x/strings#method-str-limit

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -16,7 +16,7 @@
             <div class="alert alert-light" role="alert">
                 <h6 class="alert-heading"><a class="link-dark alert-link stretched-link" href="{{ url('info', $entry->id) }}">{{ $entry->title }}</a></h6>
                 <p><small><span class="font-monospace">{{ $entry->created_at->format('d.m.Y') }}</span> {!! $entry->is_pinned ? '<i class="bi bi-pin-angle"></i>' : '' !!}</small></p>
-                <p>{!! Purify::clean(preg_split("/<br>/", $entry->text)[0]) !!}</p>
+                <p>{!! Purify::clean(Str::limit($entry->text, 200, ' (...)')) !!}</p>
             </div>
         </div>
     @endforeach
