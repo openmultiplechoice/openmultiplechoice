952cf8ad942f0202789c00e90b650c254b988355
Levin Straub <github@accounts.levinus.de>
2025-02-25 17:57:52 +0100

    Refactor and redesign comment section
    Currently, the comment section looks rather crude and the indentation
    and design doesn't work well on devices with a small screen width.
    
    Refactor and improve the functionality and design as follows:
    
    * Highlight / hide comments above / below a threshold of thumbs up /
      down
    * Reduce the indentation and use border elements to display the comment
      thread
    * Place the thumb up / down buttons and counter on the left side within
      the message element and reduce its size
    * Reduce the size of the shown metadata (reply icon, author info, etc.)
    * Use smaller buttons for Reply, Edit, etc.
    
    Also, fix a rare bug where a "deleted comment" message would be shown
    even when all replies are deleted as well.

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -13,7 +13,14 @@ class MessageController extends Controller
         $messages = Message::where([
                 ['author_id', '=', Auth::id()],
                 ['is_deleted', '=', false],
-            ])->orderByDesc('created_at', 'desc')->paginate(20);
+            ])
+            ->with('author:id,name,public_name')
+            ->withCount(['thumbs as thumbs_up_count' => function ($query) {
+                $query->where('type', 'up');
+            }, 'thumbs as thumbs_down_count' => function ($query) {
+                $query->where('type', 'down');
+            }])
+            ->orderByDesc('created_at', 'desc')->paginate(20);
         return view('messages', ['messages' => $messages]);
     }
 }
