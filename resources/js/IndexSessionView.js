adb9b7b00ec3e524d8f5afe5a41f0f4338b2607c
Michael Schubert <schu@schu.io>
2025-03-29 15:56:07 +0100

    Upgrade to Svelte 5
    Upgrade to Svelte 5 with its overhauled syntax and reactivity system:
    
    https://svelte.dev/docs/svelte/v5-migration-guide
    
    This patch is the sum of running
    
    ```
    npx sv migrate svelte-5
    ```
    
    on the `resources/` directory and the following manual updates and
    adjustments:
    
    * Remove invalid each item assignments:
      https://svelte.dev/docs/svelte/compiler-errors#each_item_invalid_assignment
    
    * Remove a now forbidden `<p><div> ... </div></p>` construct (`div`
      inside `p` element)
    
    * Add `$bindable()`  where the migration script didn't pick it up:
      https://svelte.dev/docs/svelte/v5-migration-guide#Breaking-changes-in-runes-mode-Bindings-need-to-be-explicitly-defined-using-$bindable()
    
    * Use `untrack` (https://svelte.dev/docs/svelte/svelte#untrack) where
      necessary to retain existing behavior (e.g. `currentQuestionContext`
      updates)
    
    * Add a new wrapper component `QuestionView.svelte` to ensure `question`
      is always bindable `$state` in order to continue to be able to use
      `SessionQuestionView` not only from the `SessionView` but also for
      displaying questions. (In `SessionQuestionView`, `question` is
      expected as a bindable property.)
    
    For the time being, we use some of the helper functions provided by
    `svelte/legacy`. Those will be migrated in a separate pull request (or
    pull requests) over time.
    
    Resolves #1011
    Resolves #1102

--- resources/js/IndexSessionView.js
+++ resources/js/IndexSessionView.js
@@ -1,8 +1,9 @@
 import IndexSessionView from './IndexSessionView.svelte';
+import { mount } from "svelte";
 
 const indexSessionViewEl = document.getElementById('IndexSessionView');
 
-const f = new IndexSessionView({
+const f = mount(IndexSessionView, {
     target: indexSessionViewEl,
     props: {
         userId: indexSessionViewEl.dataset.userId,
--- resources/js/SessionView.js
+++ resources/js/SessionView.js
@@ -1,8 +1,9 @@
 import SessionView from './SessionView.svelte';
+import { mount } from "svelte";
 
 const sessionViewEl = document.getElementById('SessionView');
 
-const f = new SessionView({
+const f = mount(SessionView, {
     target: sessionViewEl,
     props: {
         id: sessionViewEl.dataset.sessionId,
