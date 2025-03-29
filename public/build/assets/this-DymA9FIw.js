3b303d8d79a42a524a72828b4ca2edb21da54747
Michael Schubert <schu@schu.io>
2025-03-29 15:04:43 +0100

    npm update

---
+++ public/build/assets/this-DymA9FIw.js
@@ -0,0 +1,1 @@
+import{K as t,O as S,u as b,P as h,S as k}from"./lifecycle-BvmTvt_c.js";function u(r,i){return r===i||r?.[k]===i}function c(r={},i,a,O){return t(()=>{var f,s;return S(()=>{f=s,s=[],b(()=>{r!==a(...s)&&(i(r,...s),f&&u(a(...f),r)&&i(null,...f))})}),()=>{h(()=>{s&&u(a(...s),r)&&i(null,...s)})}}),r}export{c as b};
