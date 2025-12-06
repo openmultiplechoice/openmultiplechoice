openmultiplechoice/openmultiplechoice

commit e8315ebb36c5e63d8932936306085678fec4fe59

SessionQuestionView: add personal deck indicator
In the session view, indicate if the current question is already
included in a personal or bookmarked deck of the user by highlighting
the "AddToDeck" button with a distinct color (Bootstrap's `info` blue)
and also showing a label with the count of decks, if any (at the time of
loading the session).

A personal deck is a deck that is..

- owned by the current user,
- not public-rw-listed,
- not ephemeral and finally
- not archived.

A bookmarked deck is only shown if it's `public-rw`, since it doesn't
make sense to list decks that the current user cannot modify, i.e.
`public-ro` decks where they cannot add/remove questions.

app/Http/Controllers/Api/SessionController.php
@@ -121,7 +121,22 @@ class SessionController extends Controller
     {
         abort_if($session->user_id != Auth::id(), 404);
 
-        $deck = Deck::with('questions', 'cases', 'questions.images', 'questions.answers', 'questions.case')->find($session->deck_id);
+        $user = Auth::user();
+
+        // Load the deck with its questions, answers, images, and cases.
+        // For each question, also load the count of personal / bookmarked
+        // decks of the current user that include that question.
+        $deck = Deck::with('cases', 'questions.images', 'questions.answers', 'questions.case')
+            ->with(['questions' => function ($query) use ($user) {
+                $query->withCount(['decks as add_to_deck_included_count' => function ($q) use ($user) {
+                    $q->personalDecksBy($user)
+                    ->orWhere(function ($q2) use ($user) {
+                        $q2->bookmarkedAndWritableBy($user);
+                    });
+                }]);
+            }])
+            ->find($session->deck_id);
+
         abort_if($deck->access == "private" && $deck->user_id != Auth::id() && !Auth::user()->is_admin, 400);
 
         $session = $session->load('answerChoices');

app/Http/Controllers/Api/DeckController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
@@ -91,17 +92,11 @@ class DeckController extends Controller
 
     public function indexWithQuestionIds(Request $request)
     {
-        $user_id = Auth::id();
+        $user = Auth::user();
 
-        $decks = Deck::where([
-                ['user_id', '=', $user_id],
-                ['access', '!=', 'public-rw-listed'],
-                ['is_ephemeral', '=', false],
-                ['is_archived', '=', false],
-            ])
-            ->orWhereHas('bookmarks', function ($query) {
-                $query->where('user_id', '=', Auth::id())
-                    ->where('access', '=', 'public-rw');
+        $decks = Deck::personalDecksBy($user)
+            ->orWhere(function (Builder $query) use ($user) {
+                $query->bookmarkedAndWritableBy($user);
             })
             ->with('questions:id')->get();
 

app/Http/Controllers/DeckQuestionController.php
@@ -44,6 +44,9 @@ class DeckQuestionController extends Controller
             $urlNext = '/decks/'. $deck->id .'/questions/'. $nextQuestionId;
         }
 
+        // Get count of personal / bookmarked decks of the current user that include this question
+        $question->loadAddToDeckCount(Auth::user());
+
         return view('deck-question', [
             'deck' => $deck,
             'question' => $question,

app/Http/Controllers/ModuleController.php
@@ -65,7 +65,7 @@ class ModuleController extends Controller
 
         $decks = $module->decks
             ->filter(function ($deck) {
-                return $deck->isPublic() || $deck->isOwnedByUser(Auth::user());
+                return $deck->isPublic() || $deck->isOwnedBy(Auth::user());
             })
             ->sortBy('name')
             ->map(function ($deck) {

app/Http/Controllers/QuestionController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 use App\Models\Question;
-use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
@@ -12,6 +13,9 @@ class QuestionController extends Controller
         $question->load('answers', 'images', 'case');
 
         $decks = $question->decks()->where('access', '=', 'public-rw-listed')->get();
+
+        // Get count of personal / bookmarked decks of the current user that include this question
+        $question->loadAddToDeckCount(Auth::user());
 
         return view('question', [
             'question' => $question,

app/Models/Deck.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
@@ -71,8 +73,34 @@ class Deck extends Model
         ]);
     }
 
-    public function isOwnedByUser(User $user)
+    public function isOwnedBy(User $user)
     {
         return $this->user_id === $user->id;
+    }
+
+    /**
+     * Scope a query to only include personal decks for a user.
+     * Excludes ephemeral and archived decks.
+     */
+    #[Scope]
+    protected function personalDecksBy(Builder $query, User $user): void
+    {
+        $query->where('user_id', $user->id)
+            ->where('access', '!=', 'public-rw-listed')
+            ->where('is_ephemeral', false)
+            ->where('is_archived', false);
+    }
+
+    /**
+     * Scope a query to only include decks that are bookmarked by the user
+     * with public read-write access.
+     */
+    #[Scope]
+    protected function bookmarkedAndWritableBy(Builder $query, User $user): void
+    {
+        $query->whereHas('bookmarks', function ($query) use ($user) {
+            $query->where('user_id', '=', $user->id)
+                ->where('access', '=', 'public-rw');
+        });
     }
 }

app/Models/Question.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -50,5 +51,17 @@ class Question extends Model
         static::deleting(function ($question) {
             $question->images()->delete();
         });
+    }
+
+    public function loadAddToDeckCount(User $user): void
+    {
+        $this->add_to_deck_included_count = $this->decks()
+            ->where(function (Builder $query) use ($user) {
+                $query->personalDecksBy($user)
+                ->orWhere(function ($query) use ($user) {
+                    $query->bookmarkedAndWritableBy($user);
+                });
+            })
+            ->count();
     }
 }

public/build/assets/DeckForm-DUochZnm.js
@@ -1,1 +1,0 @@
-import{d as Y,p as j,g as e,s as O,a as C,f as I,b as M,c as b,i as F,e as p,h as K,j as c,t as w,u as N,k as dt,l as R,m as P,n as ct,o as et,q as lt}from"./if-BZUX_OG7.js";import{o as ut}from"./index-client-D2V4MdKn.js";import{s as J,a as H,p as U,e as X,i as Z}from"./event-modifiers-Cjnfc0B6.js";import{k as vt,h as $,Q as mt}from"./QuestionForm-Jvc-r6E6.js";import{h as _t}from"./html-DVYBXfyI.js";import{s as V}from"./class-BfJg_ovI.js";import{b as ft}from"./input-Dhvtgc8-.js";import{b as pt}from"./this-DNveabDO.js";import{p as at}from"./props-DtkZ_l43.js";import{r as bt}from"./legacy-client-Czxnps-C.js";import{d as ht,p as tt}from"./purify.es-8jJjCGTs.js";import"./style-BtOSwUPa.js";import"./select-Db2Y0huo.js";import"./lodash-DjGC5Q8M.js";import"./_commonjsHelpers-Cpj98o6Y.js";var gt=I('<form action="#" class="mt-3"><div class="mb-3"><label class="form-label">Case text</label> <input type="hidden"/> <trix-editor></trix-editor> <!></div></form>',2),xt=I(`<div class="alert alert-light mt-5" role="alert"><p><strong>Delete case?</strong> A case can only be deleted if it's not associated with any question.</p> <button type="button" class="btn btn-sm btn-danger"><i class="bi bi-trash"></i> Delete case</button></div>`),qt=I("<!> <!>",1);function kt(E,n){j(n,!0);let t=at(n,"kase",15),_=O(void 0),v=O("");bt(()=>{e(_)&&g(e(_))});function g(o){o.addEventListener("trix-change",function(){t(t().text=document.getElementById("caseText"+t().id).value,!0),C(v,'<p class="text-end">Saving ...</p>'),B()}),o.toolbarElement.style.display="none",o.addEventListener("trix-focus",function(i){i.target.toolbarElement.style.display="block"}),o.addEventListener("trix-blur",function(i){i.target.toolbarElement.contains(document.activeElement)||(i.target.toolbarElement.style.display="none")})}var x;function B(){x&&x.cancel(),x=ht(()=>{axios.put("/api/cases/"+t().id,t()).then(function(o){C(v,'<p class="text-end">Saved <span class="text-success-dark">&check;</span></p>')}).catch(function(o){C(v,'<p class="text-end">Saving ... <span class="text-danger-dark">failed!</span></p>'),alert(o)})},500,{maxWait:2e3}),x()}var m=qt(),q=M(m);vt(q,()=>t().id,o=>{var i=gt(),h=c(i),d=c(h),l=b(d,2),u=b(l,2);w(()=>J(u,"id",`editor-case${t().id??""}`)),V(u,1,"bg-light trix-content"),w(()=>J(u,"input",`caseText${t().id??""}`)),pt(u,k=>C(_,k),()=>e(_));var Q=b(u,2);_t(Q,()=>e(v)),w(()=>{H(d,"for",`caseText${t().id??""}`),H(l,"id",`caseText${t().id??""}`)}),ft(l,()=>t().text,k=>t(t().text=k,!0)),p(o,i)});var D=b(q,2);{var T=o=>{var i=xt(),h=b(c(i),2),d=N(()=>U(()=>{confirm("Are you sure you want to delete this case?")&&n.handleCaseRemove(t().id)}));h.__click=function(...l){e(d)?.apply(this,l)},w(l=>h.disabled=l,[()=>n.cases.some(l=>l.questions?l.questions.some(u=>u.case_id===t().id):!1)]),p(o,i)};F(D,o=>{n.handleCaseRemove&&o(T)})}p(E,m),K()}Y(["click"]);var yt=I('<button type="button" aria-current="true"><small><!></small></button>'),wt=I('<button type="button" aria-current="true"><small><i class="bi bi-clipboard2-pulse"></i> <!></small></button>'),Et=I('<div class="list-group"></div> <div class="list-group mt-2"></div>',1),Ct=I("<p>Loading ...</p>");function It(E,n){j(n,!0);let t=at(n,"data",15),_=N(()=>t()?t().questions.find(m=>m.id===t().current_question_id):null);var v=dt(),g=M(v);{var x=m=>{var q=Et(),D=M(q);X(D,21,()=>t().questions,Z,(o,i,h)=>{var d=yt(),l=N(()=>U(()=>{t(t().current_question_id=e(i).id,!0),t(t().current_case_id=null,!0)}));d.__click=function(...r){e(l)?.apply(this,r)};let u;var Q=c(d),k=c(Q);{var A=r=>{var f=R();w(S=>P(f,S),[()=>tt.sanitize(e(i).text).replace(/<\/?[^>]+(>|$)/g,"")]),p(r,f)},L=r=>{var f=R();f.nodeValue=`Question Nr. ${h+1}`,p(r,f)};F(k,r=>{e(i).text?r(A):r(L,!1)})}w(r=>u=V(d,1,"list-group-item list-group-item-action text-truncate",null,u,r),[()=>({"list-group-item-dark":e(i).id===t().current_question_id,"list-group-item-light":e(i).id!==t().current_question_id,"bg-warning-subtle":t().current_case_id&&e(i).case_id===t().current_case_id})]),p(o,d)});var T=b(D,2);X(T,21,()=>t().cases,Z,(o,i,h)=>{var d=wt(),l=N(()=>U(()=>{t(t().current_case_id=e(i).id,!0),t(t().current_question_id=null,!0)}));d.__click=function(...r){e(l)?.apply(this,r)};let u;var Q=c(d),k=b(c(Q),2);{var A=r=>{var f=R();w(S=>P(f,S),[()=>tt.sanitize(e(i).text).replace(/<\/?[^>]+(>|$)/g,"")]),p(r,f)},L=r=>{var f=R();f.nodeValue=`Case Nr. ${h+1}`,p(r,f)};F(k,r=>{e(i).text?r(A):r(L,!1)})}w(r=>u=V(d,1,"list-group-item list-group-item-action text-truncate",null,u,r),[()=>({"list-group-item-dark":e(i).id===t().current_case_id,"list-group-item-info":e(i).id!==t().current_case_id&&!(e(_)&&e(_).case_id===e(i).id),"list-group-item-warning":e(_)&&e(_).case_id===e(i).id})]),p(o,d)}),p(m,q)},B=m=>{var q=Ct();p(m,q)};F(g,m=>{t()?m(x):m(B,!1)})}p(E,v),K()}Y(["click"]);function Qt(E,n,t){var _={deck_id:n.id};axios.post("/api/cases",_).then(function(v){e(t).cases=[...e(t).cases,v.data],e(t).current_case_id=v.data.id,e(t).current_question_id=null,et().then(()=>{const g=document.getElementById("editor-case"+v.data.id);g&&g.focus()})}).catch(function(v){alert(v)}),window.scrollTo({top:0,behavior:"smooth"})}var Dt=(E,n)=>{n("mc")},Tt=(E,n)=>{n("card")},At=I('<div class="row"><div class="col"><h4 class="text-truncate"><a class="text-reset text-decoration-none"> </a></h4></div></div> <div class="sticky-top bg-white mb-3" style="z-index: 20;"><div class="row"><div class="col d-grid d-sm-block gap-2 py-3"><button type="button" class="btn btn-sm btn-primary" title="Add a new MC question (Shortcut: Alt+Q)">Add MC question</button> <button type="button" class="btn btn-sm btn-primary">Add card question</button> <button type="button" class="btn btn-sm btn-light"><i class="bi bi-clipboard2-pulse"></i> Add case</button></div></div> <div class="row"><div class="col"><div class="border-dark" style="border-bottom: dotted; border-width: 1px;"></div></div></div></div> <div class="row"><div class="col-md-4"><!></div> <div class="col-md-8"><!> <!></div></div>',1);function Lt(E,n){j(n,!0);let t=O(ct({cases:[],questions:[]})),_=0;const v=1e3;let g=N(()=>e(t)?e(t).questions.find(a=>a.id===e(t).current_question_id):null),x=N(()=>e(t)?e(t).cases.find(a=>a.id===e(t).current_case_id):null);ut(()=>{$.filter=function(){return!0},$("alt+Q",function(a){a.preventDefault();const s=Date.now();s-_>v&&(m("mc"),_=s)}),axios.get("/api/decks/"+n.id).then(function(a){C(t,a.data,!0);const s=B();s&&e(t).questions.some(y=>y.id===parseInt(s))?e(t).current_question_id=parseInt(s):e(t).questions.length>0&&(e(t).current_question_id=e(t).questions[e(t).questions.length-1].id)}).catch(function(a){alert(a)})});function B(){return new URLSearchParams(window.location.search).get("question_id")}function m(a){var s={id:null,text:"",type:a||"mc",correct_answer_id:null,images:[],answers:[]};axios.post("/api/decks/"+n.id+"/questions",s).then(function(y){s.id=y.data.id,e(t).questions=[...e(t).questions,s],e(t).current_question_id=s.id,et().then(()=>{const G=document.getElementById("editor-questionText");G&&G.focus()})}).catch(function(y){alert(y)}),window.scrollTo({top:0,behavior:"smooth"})}function q(a){axios.delete("/api/decks/"+n.id+"/questions/"+a).then(function(s){e(t).questions=e(t).questions.filter(y=>y.id!==a),e(t).questions.length>0&&(e(t).current_question_id=e(t).questions[e(t).questions.length-1].id)}).catch(function(s){alert(s)})}function D(a){axios.delete("/api/decks/"+n.id+"/cases/"+a).then(function(s){e(t).cases=e(t).cases.filter(y=>y.id!==a),e(t).questions.length>0&&(e(t).current_question_id=e(t).questions[e(t).questions.length-1].id)}).catch(function(s){alert(s)})}var T=At(),o=M(T),i=c(o),h=c(i),d=c(h),l=c(d),u=b(o,2),Q=c(u),k=c(Q),A=c(k);A.__click=[Dt,m];var L=b(A,2);L.__click=[Tt,m];var r=b(L,2);r.__click=[Qt,n,t];var f=b(u,2),S=c(f),it=c(S);It(it,{get data(){return e(t)},set data(a){C(t,a,!0)}});var st=b(S,2),W=c(st);{var nt=a=>{mt(a,{showConfigEditor:"true",showCaseSelector:"true",handleQuestionRemove:q,get question(){return e(g)},set question(s){C(g,s)},get cases(){return e(t).cases},set cases(s){e(t).cases=s}})};F(W,a=>{e(g)&&a(nt)})}var rt=b(W,2);{var ot=a=>{kt(a,{handleCaseRemove:D,get kase(){return e(x)},set kase(s){C(x,s)},get cases(){return e(t).cases},set cases(s){e(t).cases=s}})};F(rt,a=>{e(x)&&a(ot)})}w(()=>{H(h,"title",n.name),H(d,"href",`/decks/${n.id??""}`),P(l,n.name)}),p(E,T),K()}Y(["click"]);const z=document.getElementById("DeckForm");lt(Lt,{target:z,props:{id:z.dataset.deckId,name:z.dataset.deckName,moduleId:z.dataset.moduleId}});

commit 738f09ec435b9531e71238c1cf1d7215378cff18

Api/SessionController: fix performance regression in session loading
With e8315ebb36c5e63d8932936306085678fec4fe59 we added a "deck count
indicator" to the session view (if the question is part of 1 or more
personal decks, we highlight the "Add to deck" button and show a
counter), but the database query to aggregate the data is highly
inefficient due to the use of a single `OR` query, which prevents usage
of indexes and causes expensive full table scans. On big installations
with many questions and decks, the loading time is about 10x higher
(e.g. 250ms -> 2500ms).

Instead, run two separate queries without OR clauses, enabling index
usage on user_id and deck_id columns and merge the results in code.

app/Http/Controllers/Api/SessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 use App\Models\Deck;
@@ -124,31 +125,48 @@ class SessionController extends Controller
         $user = Auth::user();
 
         // Load the deck with its questions, answers, images, and cases.
-        // For each question, also load the count of personal / bookmarked
-        // decks of the current user that include that question.
         $deck = Deck::with('cases', 'questions.images', 'questions.answers', 'questions.case')
-            ->with(['questions' => function ($query) use ($user) {
-                $query->withCount(['decks as add_to_deck_included_count' => function ($q) use ($user) {
-                    $q->personalDecksBy($user)
-                    ->orWhere(function ($q2) use ($user) {
-                        $q2->bookmarkedAndWritableBy($user);
-                    });
-                }]);
-            }])
             ->find($session->deck_id);
 
         abort_if($deck->access == "private" && $deck->user_id != Auth::id() && !Auth::user()->is_admin, 400);
 
-        $session = $session->load('answerChoices');
-
-        // If all questions have been removed from the
-        // underlying deck, error out.
-        // TODO(schu): is there a better HTTP error code?
-        // TODO(schu): give the user a clue what's wrong
+        // If all questions have been removed from the underlying deck, error out.
         abort_if($deck->questions->isEmpty(), 410);
 
-        // If the sessions's current question has
-        // been deleted, set it to the first question
+        $questionIds = $deck->questions->pluck('id');
+
+        $personalDeckCounts = DB::table('deck_question')
+            ->select('question_id', DB::raw('COUNT(DISTINCT deck_id) as count'))
+            ->join('decks', 'deck_question.deck_id', '=', 'decks.id')
+            ->whereIn('deck_question.question_id', $questionIds)
+            ->where('decks.user_id', $user->id)
+            ->where('decks.access', '!=', 'public-rw-listed')
+            ->where('decks.is_ephemeral', false)
+            ->where('decks.is_archived', false)
+            ->groupBy('question_id')
+            ->pluck('count', 'question_id');
+
+        $bookmarkedDeckCounts = DB::table('deck_question')
+            ->select('question_id', DB::raw('COUNT(DISTINCT deck_question.deck_id) as count'))
+            ->join('decks', 'deck_question.deck_id', '=', 'decks.id')
+            ->join('deck_bookmark', 'decks.id', '=', 'deck_bookmark.deck_id')
+            ->whereIn('deck_question.question_id', $questionIds)
+            ->where('deck_bookmark.user_id', $user->id)
+            ->where('decks.access', 'public-rw')
+            ->where('decks.user_id', '!=', $user->id)  // Exclude own decks
+            ->groupBy('question_id')
+            ->pluck('count', 'question_id');
+
+        // Merge counts and attach to questions
+        foreach ($deck->questions as $question) {
+            $personalCount = $personalDeckCounts[$question->id] ?? 0;
+            $bookmarkedCount = $bookmarkedDeckCounts[$question->id] ?? 0;
+            $question->add_to_deck_included_count = $personalCount + $bookmarkedCount;
+        }
+
+        $session = $session->load('answerChoices');
+
+        // If the session's current question has been deleted, set it to the first question
         if (!in_array($session->current_question_id, array_column($deck->questions->toArray(), 'id'))) {
             $session->current_question_id = $deck->questions()->first()->id;
             $session->save();