b1de7617b82fc8a8c3c36525043a65b61f45af98
Michael Schubert <schu@schu.io>
2026-07-11 09:35:44 +0200

    npm update

--- public/build/assets/Messages-x45apKpZ.js
+++
@@ -1,1 +0,0 @@
-import{T as e}from"./client-CDRsLs0Q.js";import{t}from"./Messages-D3641eNm.js";var n=document.getElementById(`MessagesView`);e(t,{target:n,props:{questionId:n.dataset.questionId,questionContext:{isAnswered:!0}}});
